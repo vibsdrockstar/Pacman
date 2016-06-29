6c8c0f74298651f986f5834fcee1067f722734ca
Vibhor <bijoy.vibhor19@gmail.com>
2016-06-29 14:04:21 +0530

    Add files via upload

---
+++ GameCanvas.java
@@ -0,0 +1,58 @@
+import javax.swing.JComponent;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionAdapter;
+import java.awt.event.MouseMotionListener;
+import java.awt.event.MouseEvent;
+
+
+public final class GameCanvas extends JComponent{
+	private static final Dimension PREFERRED_SIZE=new Dimension(500,400);
+	private static final int PACMAN_RADIUS=20;
+	private int pacmanX;
+	private int pacmanY;
+	
+	public GameCanvas(){
+		pacmanX=100;
+		pacmanY=100;
+		//register listeners
+		addMouseListener(new MouseAdapter(){
+			public void mousePressed(final MouseEvent ev){
+				System.out.println("pressed-"+Thread.currentThread());
+				
+			}
+			});
+			addMouseMotionListener(new MouseMotionAdapter(){
+				public void mouseMoved(final MouseEvent ev){
+				System.out.println("moved-"+Thread.currentThread());
+				pacmanX=ev.getX();
+				pacmanY=ev.getY();
+				repaint();
+			}
+			public void mouseDragged(final MouseEvent ev){
+				System.out.println("dragged-"+Thread.currentThread());
+			}
+			});	
+		}
+	
+	
+	public Dimension getPreferredSize() {
+		System.out.println("getPreferredSize -"+Thread.currentThread());
+		return  PREFERRED_SIZE;
+	}
+	public void paintComponent(final Graphics g){
+		System.out.println("paintComponent -"+Thread.currentThread());
+		g.setColor(Color.BLACK);
+		g.fillRect(0, 0, getWidth(),getHeight());
+		g.setColor(Color.GREEN);
+	g.drawLine(0,0,getWidth(),getHeight());
+	g.setColor(Color.YELLOW);
+	g.fillArc(pacmanX-PACMAN_RADIUS, pacmanY-PACMAN_RADIUS,2*PACMAN_RADIUS, 2*PACMAN_RADIUS, 20, 320);
+	
+	}
+
+}
